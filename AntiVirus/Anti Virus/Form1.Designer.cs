d20e94dfe9de746f70dcfa17acfd5f234695af5a
IKTSERVICE\kien <kien2004@live.no>
2022-04-28 13:28:35 +0200

    Icons added, buttons and labels moved

--- AntiVirus/Anti Virus/Form1.Designer.cs
+++ AntiVirus/Anti Virus/Form1.Designer.cs
@@ -37,32 +37,44 @@
             this.lblFolder = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblSikker = new System.Windows.Forms.Label();
+            this.pctVirus = new System.Windows.Forms.PictureBox();
+            this.pctNoVirus = new System.Windows.Forms.PictureBox();
+            this.pctNeutral = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctVirus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctNoVirus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctNeutral)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Location = new System.Drawing.Point(185, 15);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(193, 197);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(250, 7);
+            this.listBox1.Location = new System.Drawing.Point(231, 13);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(301, 316);
+            this.listBox1.Size = new System.Drawing.Size(198, 199);
             this.listBox1.TabIndex = 1;
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(12, 216);
+            this.btnBrowse.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBrowse.BackgroundImage")));
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowse.FlatAppearance.BorderSize = 0;
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowse.Location = new System.Drawing.Point(326, 277);
             this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.Size = new System.Drawing.Size(52, 50);
             this.btnBrowse.TabIndex = 2;
             this.btnBrowse.Text = "Browse";
             this.btnBrowse.UseVisualStyleBackColor = true;
@@ -70,18 +82,26 @@
             // 
             // btnScan
             // 
-            this.btnScan.Location = new System.Drawing.Point(131, 216);
+            this.btnScan.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnScan.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnScan.BackgroundImage")));
+            this.btnScan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnScan.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnScan.FlatAppearance.BorderSize = 0;
+            this.btnScan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnScan.Location = new System.Drawing.Point(377, 277);
             this.btnScan.Name = "btnScan";
-            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.Size = new System.Drawing.Size(52, 50);
             this.btnScan.TabIndex = 3;
             this.btnScan.Text = "Scan";
-            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.UseVisualStyleBackColor = false;
             this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
             // 
             // lblVirus
             // 
             this.lblVirus.AutoSize = true;
-            this.lblVirus.Location = new System.Drawing.Point(12, 246);
+            this.lblVirus.BackColor = System.Drawing.Color.RoyalBlue;
+            this.lblVirus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblVirus.Location = new System.Drawing.Point(126, 239);
             this.lblVirus.Name = "lblVirus";
             this.lblVirus.Size = new System.Drawing.Size(0, 13);
             this.lblVirus.TabIndex = 4;
@@ -89,7 +109,10 @@
             // lblFolder
             // 
             this.lblFolder.AutoSize = true;
-            this.lblFolder.Location = new System.Drawing.Point(12, 278);
+            this.lblFolder.BackColor = System.Drawing.Color.RoyalBlue;
+            this.lblFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblFolder.Location = new System.Drawing.Point(19, 195);
+            this.lblFolder.MaximumSize = new System.Drawing.Size(130, 30);
             this.lblFolder.Name = "lblFolder";
             this.lblFolder.Size = new System.Drawing.Size(42, 13);
             this.lblFolder.TabIndex = 5;
@@ -97,16 +120,66 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(14, 300);
+            this.progressBar1.Location = new System.Drawing.Point(12, 255);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(193, 23);
+            this.progressBar1.Size = new System.Drawing.Size(417, 23);
             this.progressBar1.TabIndex = 6;
+            // 
+            // lblSikker
+            // 
+            this.lblSikker.AutoSize = true;
+            this.lblSikker.BackColor = System.Drawing.Color.RoyalBlue;
+            this.lblSikker.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblSikker.Location = new System.Drawing.Point(19, 237);
+            this.lblSikker.MaximumSize = new System.Drawing.Size(100, 15);
+            this.lblSikker.MinimumSize = new System.Drawing.Size(50, 15);
+            this.lblSikker.Name = "lblSikker";
+            this.lblSikker.Size = new System.Drawing.Size(50, 15);
+            this.lblSikker.TabIndex = 7;
+            this.lblSikker.Text = "[Results]";
+            // 
+            // pctVirus
+            // 
+            this.pctVirus.Image = ((System.Drawing.Image)(resources.GetObject("pctVirus.Image")));
+            this.pctVirus.Location = new System.Drawing.Point(12, 38);
+            this.pctVirus.Name = "pctVirus";
+            this.pctVirus.Size = new System.Drawing.Size(163, 131);
+            this.pctVirus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctVirus.TabIndex = 8;
+            this.pctVirus.TabStop = false;
+            // 
+            // pctNoVirus
+            // 
+            this.pctNoVirus.Image = ((System.Drawing.Image)(resources.GetObject("pctNoVirus.Image")));
+            this.pctNoVirus.Location = new System.Drawing.Point(12, 38);
+            this.pctNoVirus.Name = "pctNoVirus";
+            this.pctNoVirus.Size = new System.Drawing.Size(163, 131);
+            this.pctNoVirus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctNoVirus.TabIndex = 9;
+            this.pctNoVirus.TabStop = false;
+            // 
+            // pctNeutral
+            // 
+            this.pctNeutral.Image = ((System.Drawing.Image)(resources.GetObject("pctNeutral.Image")));
+            this.pctNeutral.Location = new System.Drawing.Point(12, 38);
+            this.pctNeutral.Name = "pctNeutral";
+            this.pctNeutral.Size = new System.Drawing.Size(163, 131);
+            this.pctNeutral.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctNeutral.TabIndex = 10;
+            this.pctNeutral.TabStop = false;
             // 
             // frmVCKS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 335);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(441, 335);
+            this.Controls.Add(this.pctNeutral);
+            this.Controls.Add(this.pctNoVirus);
+            this.Controls.Add(this.pctVirus);
+            this.Controls.Add(this.lblSikker);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.lblFolder);
             this.Controls.Add(this.lblVirus);
@@ -114,9 +187,14 @@
             this.Controls.Add(this.btnBrowse);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmVCKS";
             this.Text = "VCKS Anti Virus";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctVirus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctNoVirus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctNeutral)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,6 +210,10 @@
         private System.Windows.Forms.Label lblFolder;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblSikker;
+        private System.Windows.Forms.PictureBox pctVirus;
+        private System.Windows.Forms.PictureBox pctNoVirus;
+        private System.Windows.Forms.PictureBox pctNeutral;
     }
 }
 
--- AntiVirus/Anti Virus/Form1.cs
+++ AntiVirus/Anti Virus/Form1.cs
@@ -7,14 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Anti_Virus
 {
-    public partial class Form1 : Form
+    public partial class frmVCKS : Form
     {
-        public Form1()
+        int viruses;
+        string[] virusList = new string[] { "virus", "trojan", "hack", "hacker" };
+        public frmVCKS()
         {
             InitializeComponent();
+            pctVirus.Hide();
+            pctNoVirus.Hide();
+            pctNeutral.Show();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            lblFolder.Text = folderBrowserDialog1.SelectedPath;
+            viruses = 0;
+            lblVirus.Text = "Viruses : " + viruses.ToString();
+            progressBar1.Value = 0;
+            listBox1.Items.Clear();
+            pctNeutral.Show();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            List<string> search = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.*").ToList();
+            progressBar1.Maximum = search.Count;
+            foreach(string item in search)
+            {
+                try
+                {
+                    StreamReader stream = new StreamReader(item);
+                    string read = stream.ReadToEnd();
+
+                    foreach(string st in virusList)
+                    {
+                        if(Regex.IsMatch(read,st))
+                        {
+                            viruses += 1;
+                            lblVirus.Text = "Viruses : " + viruses.ToString();
+                            listBox1.Items.Add(item);
+
+                        }
+                        progressBar1.Increment(1);
+
+                        if(viruses <= 0)
+                        {
+                            lblSikker.Text = "Filene er sikker!";
+                            pctVirus.Hide();
+                            pctNeutral.Hide();
+                            pctNoVirus.Show();
+                        }
+
+                        if (viruses >= 1)
+                        {
+                            lblSikker.Text = "Filene er usikker!";
+                            pctNoVirus.Hide();
+                            pctNeutral.Hide();
+                            pctVirus.Show();
+                        }
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                //
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
